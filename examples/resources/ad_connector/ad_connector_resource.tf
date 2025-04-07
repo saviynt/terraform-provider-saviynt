7c6af60d63c41c7918822c982d52dcbe0f2129d7
shaleenshukla <i-shaleen.shukla>
2025-04-07 14:21:05 +0530

    Fix the read functionality for the ad connector.

--- examples/resources/ad_connector/ad_connector_resource.tf
+++ examples/resources/ad_connector/ad_connector_resource.tf
@@ -14,7 +14,7 @@ provider "saviynt" {
 
 resource "saviynt_ad_connection_resource" "ss" {
   connection_type     = "AD"
-  connection_name     = "dellwireless"
+  connection_name     = "dellwireles3490123"
   url                 = format("%s://%s:%d", var.LDAP_PROTOCOL, var.IP_ADDRESS, var.LDAP_PORT)
   username            = var.BIND_USER
   password=var.PASSWORD
@@ -22,23 +22,11 @@ resource "saviynt_ad_connection_resource" "ss" {
   vault_configuration = var.VAULT_CONFIG
   save_in_vault       = var.SAVE_IN_VAULT
 
-  enable_account_json=jsonencode(
-{
-  "USEDNFROMACCOUNT": "YES",
-  "MOVEDN": "NO",
-  "REMOVEGROUPS": "NO",
-  "ENABLEACCOUNTOU": "CN=Users,DC=corp,DC=SaviyntAD,DC=com",
-  "AFTERMOVEACTIONS": {
-    "userAccountControl": "544",
-    "otherMailbox": [
-      "$${user?.customproperty15.toString().replace(',','\",\"')}"
-    ]
-  }
-})
+
    account_name_rule="CN=$${user.username},CN=Users,DC=corp,DC=SaviyntAD,DC=com"
-   account_attribute="[CUSTOMPROPERTY1::CN#String,CUSTOMPROPERTY2::userPrincipalName#String,LASTLOGONDATE::lastLogon#millisec,DISPLAYNAME::displayName#String,CUSTOMPROPERTY25::company#String,CUSTOMPROPERTY3::sn#String,COMMENTS::distinguishedName#String,CUSTOMPROPERTY4::homeDirectory#String,LASTPASSWORDCHANGE::pwdLastSet#millisec,CUSTOMPROPERTY5::co#String,CUSTOMPROPERTY6::cn#String,CUSTOMPROPERTY7::givenName#String,CUSTOMPROPERTY8::title#String,CUSTOMPROPERTY9::telephoneNumber#String,CUSTOMPROPERTY10::c#String,DESCRIPTION::description#String,CUSTOMPROPERTY11::uSNCreated#String,VALIDTHROUGH::accountExpires#millisec,CUSTOMPROPERTY12::logonCount#String,CUSTOMPROPERTY13::physicalDeliveryOfficeName#String,UPDATEDATE::whenChanged#date,CUSTOMPROPERTY14::extensionAttribute1#String,CUSTOMPROPERTY15::extensionAttribute2#String,CUSTOMPROPERTY16::streetAddress#String,CUSTOMPROPERTY17::mailNickname#String,CUSTOMPROPERTY18::department#String,CUSTOMPROPERTY19::countryCode#String,NAME::name#String,CUSTOMPROPERTY21::manager#String,CUSTOMPROPERTY22::homePhone#String,CUSTOMPROPERTY23::mobile#String,CREATED_ON::whenCreated#date,ACCOUNTCLASS::objectClass#String,ACCOUNTID::distinguishedName#String,CUSTOMPROPERTY24::userAccountControl#String,STATUS::userAccountControl#Number,CUSTOMPROPERTY26::objectGUID#Binary,CUSTOMPROPERTY27::objectSid#Binary]"
+   account_attribute="[CUSTOMPROPERTY1::samaccountname#String,CUSTOMPROPERTY2::userprincipalname#String,LASTLOGONDATE::lastLogon#millisec,DISPLAYNAME::cn#String,CUSTOMPROPERTY25::company#String,CUSTOMPROPERTY3::sn#String,COMMENTS::distinguishedname#String,CUSTOMPROPERTY4::homedirectory#String,LASTPASSWORDCHANGE::pwdlastset#millisec,CUSTOMPROPERTY5::co#String,CUSTOMPROPERTY6::cn#String,CUSTOMPROPERTY7::givenname#String,CUSTOMPROPERTY8::title#String,CUSTOMPROPERTY9::telephonenumber#String,CUSTOMPROPERTY10::c#String,DESCRIPTION::description#String,CUSTOMPROPERTY11::usncreated#String,VALIDTHROUGH::accountexpires#millisec,CUSTOMPROPERTY12::logoncount#String,CUSTOMPROPERTY13::physicaldeliveryofficename#String,UPDATEDATE::whenchanged#date,CUSTOMPROPERTY14::extensionattribute1#String,CUSTOMPROPERTY15::extensionattribute2#String,CUSTOMPROPERTY16::streetaddress#String,CUSTOMPROPERTY17::mailnickname#String,CUSTOMPROPERTY18::department#String,CUSTOMPROPERTY19::countrycode#String,NAME::distinguishedname#String,CUSTOMPROPERTY21::manager#String,CUSTOMPROPERTY22::homephone#String,CUSTOMPROPERTY23::mobile#String,ACCOUNTCLASS::objectclass#String,ACCOUNTID::distinguishedname#String,CUSTOMPROPERTY24::useraccountcontrol#String,status::useraccountcontrol#Number,CUSTOMPROPERTY26::objectguid#String,CUSTOMPROPERTY28::forest#String,CUSTOMPROPERTY29::domain#string,CUSTOMPROPERTY30::objectclass#String]"
    entitlement_attribute="memberOf"
-   user_attribute="[CUSTOMPROPERTY1::CN#String,USERNAME::name#String,DISPLAYNAME::displayName#String,CUSTOMPROPERTY25::company#String,CUSTOMPROPERTY3::sn#String,COMMENTS::distinguishedName#String,CUSTOMPROPERTY4::homeDirectory#String,CUSTOMPROPERTY5::co#String,CUSTOMPROPERTY6::cn#String,CUSTOMPROPERTY7::givenName#String,CUSTOMPROPERTY8::title#String,CUSTOMPROPERTY9::telephoneNumber#String,CUSTOMPROPERTY10::c#String,CUSTOMPROPERTY11::uSNCreated#String,ENDDATE::accountExpires#millisec,CUSTOMPROPERTY12::logonCount#String,CUSTOMPROPERTY13::physicalDeliveryOfficeName#String,UPDATEDATE::whenChanged#date,CUSTOMPROPERTY14::extensionAttribute1#String,CUSTOMPROPERTY15::extensionAttribute2#String,CUSTOMPROPERTY16::streetAddress#String,CUSTOMPROPERTY17::mailNickname#String,CUSTOMPROPERTY18::department#String,CUSTOMPROPERTY19::countryCode#String,CUSTOMPROPERTY2::sAMAccountName#String,CUSTOMPROPERTY20::userPrincipalName#String,CUSTOMPROPERTY21::manager#String,CUSTOMPROPERTY22::homePhone#String,CUSTOMPROPERTY23::mobile#String,CREATEDATE::whenCreated#date,customproperty24::userAccountControl#String,STATUSKEY::userAccountControl#Number]"
+   user_attribute="[FIRSTNAME::givenname#String,LASTNAME::sn#String,CUSTOMPROPERTY1::samaccountname#String,USERNAME::distinguishedname#String,DISPLAYNAME::cn#String,CUSTOMPROPERTY25::description#String,CUSTOMPROPERTY3::sn#String,COMMENTS::distinguishedname#String,CUSTOMPROPERTY4::homedirectory#String,CUSTOMPROPERTY5::co#String,CUSTOMPROPERTY6::cn#String,CUSTOMPROPERTY7::givenname#String,CUSTOMPROPERTY8::title#String,CUSTOMPROPERTY9::telephonenumber#String,CUSTOMPROPERTY10::c#String,CUSTOMPROPERTY11::uSNCreated#String,ENDDATE::accountExpires#millisec,CUSTOMPROPERTY12::logonCount#String,CUSTOMPROPERTY13::physicaldeliveryofficename#String,UPDATEDATE::whenchanged#date,CUSTOMPROPERTY14::extensionattribute1#String,CUSTOMPROPERTY15::extensionattribute2#String,CUSTOMPROPERTY16::streetaddress#String,CUSTOMPROPERTY17::mailnickname#String,CUSTOMPROPERTY18::department#String,CUSTOMPROPERTY19::countrycode#String,CUSTOMPROPERTY2::samaccountname#String,CUSTOMPROPERTY20::userprincipalname#String,CUSTOMPROPERTY21::manager#String,CUSTOMPROPERTY22::homephone#String,CUSTOMPROPERTY23::mobile#String,CREATEDATE::whencreated#date,customproperty24::useraccountcontrol#String,CUSTOMPROPERTY26::distinguishedname#String,statuskey::useraccountcontrol#String,CUSTOMPROPERTY27::objectguid#String,RECONCILATION_FIELD::CUSTOMPROPERTY27,CUSTOMPROPERTY28::forest#String,CUSTOMPROPERTY29::domain#string]"
    endpoints_filter=jsonencode({"AD_Child":[{"memberOf":["CN=QAGroup01,DC=corp,DC=SaviyntAD,DC=com"]}]})
    update_user_json=jsonencode({ "sn": "$${user.lastname}"})
       status_key_json=jsonencode(
@@ -51,8 +39,19 @@ resource "saviynt_ad_connection_resource" "ss" {
          ]
  }
    )
-   group_search_base_dn="DC=corp,DC=SaviyntAD,DC=com"
-  create_update_mappings = "chinatown"
+  
+  create_update_mappings = jsonencode({
+    "cn": "$${role?.customproperty27}",
+"objectCategory": "CN=Group,CN=Schema,CN=Configuration,DC=corp,DC=SaviyntAD,DC=com",
+"distinguishedName": "$${role?.role_name}",
+"displayName": "$${role?.displayname}",
+"SamAccountName": "$${role?.customproperty27}",
+"description": "$${role?.description}",
+"objectClass": "group",
+"name": "$${role?.customproperty27}",
+"managedBy":"$${user?.comments}"
+  }
+  )
   group_import_mapping=jsonencode({
   "importGroupHierarchy": "true",
   "entitlementTypeName": "",
@@ -63,6 +62,23 @@ resource "saviynt_ad_connection_resource" "ss" {
   "entitlementOwnerAttribute": "managedBy",
   "tableFieldAttribute": "accountID"
 })
-objectfilter="(&(objectCategory=person)(objectClass=user))"
-searchfilter="CN=Users,DC=corp,DC=SaviyntAD,DC=com"
+objectfilter=jsonencode(
+  {"full":"(&(objectCategory=person)(objectClass=user)(sAMAccountName=*))","incremental":"(&(objectCategory=person)(objectClass=user)(sAMAccountName=*))"}
+)
+searchfilter="OU=CONNQA,OU=SaviyntTeams,DC=saviyntlabs,DC=org"
+create_account_json=jsonencode(
+ {"samaccountname":"$${task.accountName}","sn":"$${user.lastname}","displayName":"$${user.displayname}","cn":"$${cn}","objectclass":["top","person","organizationalPerson","user"],  "userAccountControl":"544","givenName":"$${user.firstname}","name":"$${user.displayname}"}
+)
+status_threshold_config = jsonencode({
+  statusAndThresholdConfig = {
+    statusColumn               = "customproperty24"
+    activeStatus              = ["512", "544"]
+    deleteLinks               = false
+    accountThresholdValue     = 1000
+    correlateInactiveAccounts = true
+    inactivateAccountsNotInFile = false
+  }
+})
+
+
 }
