3d3d888c8ff9ac53a2260387813bf7aa040f8eb1
shaleenshukla <i-shaleen.shukla>
2025-04-15 10:18:42 +0530

    Fix the security system unsetting of data in resource

--- examples/resources/saviynt_entraid_connection_resource/resource.tf
+++ examples/resources/saviynt_entraid_connection_resource/resource.tf
@@ -1,6 +1,6 @@
 resource "saviynt_entraid_connection_resource" "ss" {
   connection_type = "AzureAD"
-  connection_name = "test"
+  connection_name = "namefortheconnection"
   client_secret   = "XXXX"
   aad_tenant_id   = "XXXX"
   client_id       = "XXXX"
