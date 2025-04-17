ae6547efc9e9f122bf5527d6499d99968c259f15
Shaleen Shukla <i-shaleen.shukla@saviynt.com>
2025-04-17 14:14:35 +0530

    TER-27: Added Unix resource in Terraform (#30)
    * Added Unix code
    
    * Fixed the code for Unix resource
    
    * Changed the github.com/saviynt/saviynt-api-go-client version
    
    * Added the logs
    
    ---------
    
    Co-authored-by: shaleenshukla <i-shaleen.shukla>

---
+++ examples/resources/saviynt_unix_connection_resource/variable.tf
@@ -0,0 +1,13 @@
+variable "SAVIYNT_SERVER_URL" {
+  type        = string
+  description = "Saviynt API Server URL (without https://)"
+}
+variable "SAVIYNT_USERNAME" {
+  type        = string
+  description = "Saviynt API Username"
+}
+variable "SAVIYNT_PASSWORD" {
+  type        = string
+  description = "Saviynt API Password"
+  sensitive   = true
+}
