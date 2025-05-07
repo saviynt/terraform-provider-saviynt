9d7d6cbf599ee257c06e0e187562d6ffed63161f
shaleenshukla <i-shaleen.shukla>
2025-05-07 19:55:49 +0530

    Added guard rail for already exist connection name in create block in ADSI connector resource

--- internal/provider/ADSI_connection_resource.go
+++ internal/provider/ADSI_connection_resource.go
@@ -375,6 +375,18 @@ func (r *adsiConnectionResource) Create(ctx context.Context, req resource.Create
 	cfg.Scheme = "https"
 	cfg.AddDefaultHeader("Authorization", "Bearer "+r.token)
 	cfg.HTTPClient = http.DefaultClient
+	apiClient := openapi.NewAPIClient(cfg)
+	reqParams := openapi.GetConnectionDetailsRequest{}
+	reqParams.SetConnectionname(plan.ConnectionName.ValueString())
+	existingResource, _, err := apiClient.ConnectionsAPI.GetConnectionDetails(ctx).GetConnectionDetailsRequest(reqParams).Execute()
+	if err != nil {
+		log.Printf("Problem with the get function in Create block")
+	}
+	if existingResource != nil && existingResource.WorkdayConnectionResponse != nil && existingResource.WorkdayConnectionResponse.Errorcode != nil && *existingResource.WorkdayConnectionResponse.Errorcode == 0 {
+		log.Printf("[ERROR] Connection name already exists. Please import or use a different name")
+		resp.Diagnostics.AddError("API Create Failed", "Connection name already exists. Please import or use a different name")
+		return
+	}
 
 	if plan.EntitlementAttribute.IsNull() || plan.EntitlementAttribute.IsUnknown() {
 		plan.EntitlementAttribute = types.StringValue("memberOf")
@@ -443,7 +455,6 @@ func (r *adsiConnectionResource) Create(ctx context.Context, req resource.Create
 	}
 
 	// Initialize API client
-	apiClient := openapi.NewAPIClient(cfg)
 	apiResp, _, err := apiClient.ConnectionsAPI.CreateOrUpdate(ctx).CreateOrUpdateRequest(adsiConnRequest).Execute()
 	if err != nil || *apiResp.ErrorCode != "0" {
 		log.Printf("[ERROR] Failed to create API resource. Error: %v", err)
