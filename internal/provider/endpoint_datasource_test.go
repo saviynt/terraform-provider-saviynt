e988efa853c597c467c8611f90f658c1cbb05822
nandini-043 <i-nandini.choukimath@saviynt.com>
2025-05-13 11:55:56 +0530

    TER-37: Added Test for endpoint datasource in terraform (#64)
    * TER-33: Added test for security system and docs for unix and github rest
    
    * Added properties file to contain descriptions
    
    * TER-35: Added Test for Security System Data Source in Terraform
    
    * TER-40: Added Test for AD Connection Resource in Terraform
    
    * Added Import function to github rest resource and added test
    
    * Added import function and test for SAP connection resource
    
    * Added import function and test for Salesforce Connector in Terraform
    
    * Added basic tests for create, update, import, connection name and type change validation for Salesforce Connection resource in Terraform
    
    * Added test for create, update, import, connection name and type change validation for Saleforce connector resource
    
    * TER-62: Added Test for Salesforce Connector in Terraform
    
    * TER-59: Added Test for Workday Connector Resource in Terraform
    
    * TER-61: Added Test for SAP Connector Resource in Terraform
    
    * Added resource name attribute to the test files
    
    * TER-57: Added Test for Github REST connector resource in Terraform
    
    * Modified test file
    
    * TER-58: Added Test for Unix Connection resource in Terraform
    
    * Modified endpoint resource in Terraform with more attributes and read function
    
    * Updated Endpoint datasource in terraform
    
    * TER-34: Added test for endpoint resource in terraform
    
    * TER-37: Added test for Endpoint datasource in terraform

---
+++ internal/provider/endpoint_datasource_test.go
@@ -0,0 +1,81 @@
+package provider
+
+import (
+	"fmt"
+	"os"
+	"terraform-provider-Saviynt/util"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+)
+
+func TestAccEndpointDataSource(t *testing.T) {
+	filePath := "endpoint_resource_test_data.json"
+	createCfg := util.LoadConnectorData(t, filePath, "create")
+	datasource := "data.saviynt_endpoints_datasource.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSaviyntEndpointDataSourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					func(s *terraform.State) error {
+						res, ok := s.RootModule().Resources[datasource]
+						if !ok {
+							t.Fatalf("Resource %s not found in state", datasource)
+						}
+						t.Logf("Full data source attributes:\n%+v", res.Primary.Attributes)
+						return nil
+					},
+					// Now assert values
+					resource.TestCheckResourceAttr(datasource, "message", "Success"),
+					resource.TestCheckResourceAttr(datasource, "error_code", "0"),
+					resource.TestCheckResourceAttr(datasource, "results.0.endpointname", createCfg["endpoint_name"]),
+					resource.TestCheckResourceAttr(datasource, "results.0.display_name", createCfg["display_name"]),
+					resource.TestCheckResourceAttr(datasource, "results.0.securitysystem", createCfg["security_system"]),
+					resource.TestCheckResourceAttr(datasource, "results.0.custom_property_1", createCfg["custom_property1"]),
+					resource.TestCheckResourceAttr(datasource, "results.0.account_custom_property_1_label", createCfg["account_custom_property_1_label"]),
+					resource.TestCheckResourceAttr(datasource, "results.0.custom_property_31_label", createCfg["custom_property31_label"]),
+					resource.TestCheckResourceAttr(datasource, "results.0.enable_copy_access", createCfg["enable_copy_access"]),
+				),
+			},
+		},
+	})
+}
+
+func testAccSaviyntEndpointDataSourceConfig() string {
+	jsonPath := "${filepath}/endpoint_resource_test_data.json"
+	return fmt.Sprintf(`
+provider "saviynt" {
+  server_url = "%s"
+  username   = "%s"
+  password   = "%s"
+}
+
+locals {
+  cfg = jsondecode(file("%s"))["create"]
+}
+
+resource "saviynt_endpoint_resource" "e" {
+  endpoint_name                      = local.cfg.endpoint_name
+  display_name                       = local.cfg.display_name
+  security_system                    = local.cfg.security_system
+  custom_property1            		 = local.cfg.custom_property1
+  account_custom_property_1_label 	 = local.cfg.account_custom_property_1_label
+  custom_property31_label 			 = local.cfg.custom_property31_label
+  enable_copy_access				 = local.cfg.enable_copy_access
+}
+  
+data "saviynt_endpoints_datasource" "test" {
+	endpointname = local.cfg.endpoint_name
+	depends_on = [saviynt_endpoint_resource.e]
+}
+`, os.Getenv("SAVIYNT_URL"),
+		os.Getenv("SAVIYNT_USERNAME"),
+		os.Getenv("SAVIYNT_PASSWORD"),
+		jsonPath,
+	)
+}
